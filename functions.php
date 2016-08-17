ebc2f381e27b5e039987b48350a27466a1568d2c
Andrew Minion <andrew@andrewrminion.com>
2016-08-17 14:23:09 -0400

    remove leftover $custom_styles from copied functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,5 @@
 function add_custom_css() {
     wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/style.min.css' );
     wp_dequeue_style( 'twentysixteen-style' );
-
-    wp_add_inline_style( 'theme-style', $custom_styles );
 }
 add_action( 'wp_enqueue_scripts', 'add_custom_css', 50 );
